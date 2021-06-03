e963b41bdb0fd3c83ab5f95e5e1d1eaf1a53f0d5
aaron <aaron.staes@gmail.com>
2021-06-03 15:42:37 +0200

    Added gitignore entry for config file with API key

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,5 @@
+import {config} from '../config';
+
 const loadContactPage = () => {
     const content = document.getElementById('content');
     const contactContainer = document.createElement('div');
@@ -11,7 +13,7 @@ const loadContactPage = () => {
 
     // Create the script tag, set the appropriate attributes
     var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCWN5lN6vpa1BIygvtAACh6wX7zuaR5vAI&callback=initMap';
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${config.apiKey}&callback=initMap`;
     script.async = true;
 
     // Attach your callback function to the `window` object
