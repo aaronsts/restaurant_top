87625b9e80eadd1f9eaebf67cb9c9eadc1502741
aaron <aaron.staes@gmail.com>
2021-06-03 15:36:36 +0200

    Added Google map in Contact section

--- src/contact.js
+++ src/contact.js
@@ -2,12 +2,55 @@ const loadContactPage = () => {
     const content = document.getElementById('content');
     const contactContainer = document.createElement('div');
 
-    contactContainer.id = 'contact-container';
-    const test = document.createElement('h1');
-    test.textContent = 'Contact';
+    const h2Title = document.createElement('h2');
+    const contactCard = document.createElement('div');
+    const h3Title = document.createElement('h3');
+    const para1 = document.createElement('p')
+    const para2 = document.createElement('p');
+    const mapCard = document.createElement('div');
 
-    contactContainer.appendChild(test);
-    content.appendChild(contactContainer)
+    // Create the script tag, set the appropriate attributes
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCWN5lN6vpa1BIygvtAACh6wX7zuaR5vAI&callback=initMap';
+    script.async = true;
+
+    // Attach your callback function to the `window` object
+    window.initMap = function() {
+    // JS API is loaded and available
+    // The location of Uluru
+    const brussels = { lat: 50.84965958068527, lng: 4.347751332061692 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 14,
+      center: brussels,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: brussels,
+      map: map,
+    });
+    };
+
+    // Append the 'script' element to 'head'
+    document.head.appendChild(script);
+
+
+    contactContainer.id = 'contact-container';
+    h2Title.textContent = 'Contact';
+    h3Title.textContent = 'Phone & Email';
+    para1.textContent = '0474 59 87 62';
+    para2.textContent = 'palma@notreal.com';
+    mapCard.id = 'map';
+    contactCard.id = 'contact';
+
+    contactContainer.appendChild(h2Title);
+    contactCard.appendChild(h3Title);
+    contactCard.appendChild(para1);
+    contactCard.appendChild(para2);
+
+    contactContainer.appendChild(contactCard);
+    contactContainer.appendChild(mapCard);
+    content.appendChild(contactContainer);
 }
 
 export {
--- src/menu.js
+++ src/menu.js
@@ -2,11 +2,23 @@ const loadMenuPage = () => {
     const content = document.getElementById('content');
     const menuContainer = document.createElement('div');
 
-    menuContainer.id = 'menu-container';
-    const test = document.createElement('h1');
-    test.textContent = 'Menu';
+    const h2Title = document.createElement('h2');
+    const contactCard = document.createElement('div');
+    const h3Title = document.createElement('h3');
+    const para1 = document.createElement('p')
+    const para2 = document.createElement('p');
 
-    menuContainer.appendChild(test);
+    menuContainer.id = 'menu-container';
+    h2Title.textContent = 'Menu';
+    h3Title.textContent = 'Phone & Email'
+    para1.textContent = '0474 59 87 62';
+    para2.textContent = 'palma@notreal.com';
+
+    menuContainer.appendChild(h2Title);
+    menuContainer.appendChild(h3Title);
+    menuContainer.appendChild(para1);
+    menuContainer.appendChild(para2);
+
     content.appendChild(menuContainer)
 }
 
