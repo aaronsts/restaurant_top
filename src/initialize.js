fdfdcd0cc702698b047fcef219ea1c1e5af32e03
aaron <aaron.staes@gmail.com>
2021-06-04 09:30:56 +0200

    Changed msg

--- src/initialize.js
+++ src/initialize.js
@@ -41,7 +41,7 @@ const onPageLoad = () => {
     homeItem.textContent = 'home';
     menuItem.textContent = 'menu';
     contactItem.textContent = 'contact';
-    h2Title.textContent = 'Het Ontstaan';
+    h2Title.textContent = 'Welcome';
     para1.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     h3Title.textContent = 'Chef';
     para2.textContent = 'I love to cook vegan meals!'
